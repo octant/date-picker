ea34a45a41bbf1c14b4e6c9d364b5edb8943cba3
Michael Wood <mwood@data-crush.com>
2018-03-23 00:53:13 -0400

    Added test for next month

--- src/lib/calendar-fns.js
+++ src/lib/calendar-fns.js
@@ -11,6 +11,23 @@ const previous = (year, month, offset) => {
   }
 }
 
+const next = (year, month, offset) => {
+  const firstDayIndex = new Date(year, month + 1, 1).getDay()
+  const previousMonthFill = (0 - 7) % 7
+  const shift = firstDayIndex + previousMonthFill - offset
+
+  if (new Date(year, month + 1, 1 - shift) > new Date(year, month + 1, 1)) {
+    console.log(shift)
+    return new Date(year, month + 1, 1 - shift - 7)
+  } else {
+    return new Date(year, month + 1, 1 - shift)
+  }
+}
+
 export const getPreviousMonth = (date, offset = 0) => {
   return previous(date.getFullYear(), date.getMonth(), offset)
+}
+
+export const getNextMonth = (date, offset = 0) => {
+  return next(date.getFullYear(), date.getMonth(), offset)
 }
--- src/lib/calendar-fns.test.js
+++ src/lib/calendar-fns.test.js
@@ -1,6 +1,6 @@
 /* eslint-env jest */
 
-import {getPreviousMonth} from './calendar-fns'
+import {getPreviousMonth, getNextMonth} from './calendar-fns'
 
 describe('Previous month function', () => {
   test('gets the first date to display for the previous month', () => {
@@ -26,5 +26,32 @@ describe('Previous month function', () => {
 
     expect(firstDay.getDate()).toBe(26)
     expect(firstDay.getMonth()).toBe(1)
+  })
+})
+
+describe('Next month function', () => {
+  test('gets the first date to display for the next month', () => {
+    const date = new Date(2018, 2, 23)
+    const firstDay = getNextMonth(date)
+
+    expect(firstDay.getDate()).toBe(1)
+    expect(firstDay.getMonth()).toBe(3)
+  })
+
+  test('gets the first date using offset when month begins on offset 1', () => {
+    const date = new Date(2017, 11, 18)
+    const firstDay = getNextMonth(date, 2)
+
+    expect(firstDay.getDate()).toBe(26)
+    expect(firstDay.getMonth()).toBe(11)
+    expect(firstDay.getFullYear()).toBe(2017)
+  })
+
+  test('gets the first date to display for the next month using an offset', () => {
+    const date = new Date(2018, 3, 23)
+    const firstDay = getNextMonth(date, 1)
+
+    expect(firstDay.getDate()).toBe(30)
+    expect(firstDay.getMonth()).toBe(3)
   })
 })
