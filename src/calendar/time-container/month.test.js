64f29cfeefd7fe37af3585b83761cec710af0fd1
Michael Wood <mdwood@algomapublichealth.com>
2018-03-27 13:01:30 -0400

    Modified test to reflect change

--- src/calendar/time-container/month.test.js
+++ src/calendar/time-container/month.test.js
@@ -3,13 +3,9 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
 import serializer from 'jest-glamor-react'
-import sinon from 'sinon'
-import Enzyme, {mount} from 'enzyme'
-import Adaptor from 'enzyme-adapter-react-16'
 
 import Month from './'
 
-Enzyme.configure({adapter: new Adaptor()})
 expect.addSnapshotSerializer(serializer)
 
 describe('<Month /> appearance', () => {
@@ -21,8 +17,4 @@ describe('<Month /> appearance', () => {
     let tree = component.toJSON()
     expect(tree).toMatchSnapshot()
   })
-})
-
-describe('<Month /> functionality', () => {
-  test('has list of days')
 })
