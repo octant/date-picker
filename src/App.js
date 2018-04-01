c7de4b77c4c47b7a00d6d70e1896c21f01959475
Michael Wood <mwood@data-crush.com>
2018-04-01 18:43:01 -0400

    Removed old component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,11 @@
 import React, { Component } from 'react'
-import Calendar from './calendar'
-import Calendar2 from './calendar/calendar'
+import Calendar from './calendar/calendar'
 
 import './App.css'
 
 class App extends Component {
   render () {
-    const style = {
-      width: '10em',
-      color: 'pink',
-      position: 'relative'
-    }
-    return (
-      <div>
-        <div className='wtf' stlyle={style}>
-          <Calendar2 />
-        </div>
-        <hr />
-        <div stlyle={style}>
-          <Calendar selected='2018-02-04' />
-        </div>
-      </div>
-    )
+    return (<Calendar />)
   }
 }
 
