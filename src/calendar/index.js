285165e5054c95b1c0e2e437d6f2c96510691624
Michael Wood <mdwood@algomapublichealth.com>
2018-04-02 13:42:56 -0400

    Moved month, year,  and decade to a common timespan

--- src/calendar/index.js
+++ src/calendar/index.js
@@ -1,56 +1,31 @@
 import React from 'react'
 
 import { Container } from './styles'
-import Month from './month'
-import Year from './year'
-import Decade from './decade'
 import TimeContainer from './time-container'
 import TimeMachine from './time-machine'
+import TimeSpan from './time-span'
 
 class Calendar extends React.Component {
-  show (state) {
-    const {controls, mode, selected, startOfMonth, startOfCalendar, today} = state
-
-    switch (mode) {
-      case 'decade':
-        return <Decade
-          currentDate={startOfMonth}
-          startDate={startOfCalendar}
-          selected={selected}
-          today={today}
-          clickMethod={controls.selectDate}
-          modeMethod={controls.selectMode}
-          travelTo={controls.travelTo} />
-      case 'year':
-        return <Year
-          currentDate={startOfMonth}
-          startDate={startOfCalendar}
-          selected={selected}
-          today={today}
-          clickMethod={controls.selectDate}
-          modeMethod={controls.selectMode}
-          travelTo={controls.travelTo} />
-      default:
-        return <Month
-          currentDate={startOfMonth}
-          startDate={startOfCalendar}
-          selected={selected}
-          today={today}
-          clickMethod={controls.selectDate}
-          modeMethod={controls.selectMode}
-          travelTo={controls.travelTo} />
-    }
-  }
-
   render () {
     return (
       <TimeMachine mode={this.props.mode} selected={this.props.selected}>
         {tmState => {
+          const {controls, mode, selected, startOfMonth, startOfCalendar, today} = tmState
           return (
             <Container>
               <button onClick={tmState.controls.today}>Today</button>
               <TimeContainer>
-                {this.show(tmState)}
+                <TimeSpan
+                  span={mode}
+                  startOfMonth={startOfMonth}
+                  startOfCalendar={startOfCalendar}
+                  today={today}
+                  selected={selected}
+                  controls={{
+                    select: controls.selectDate,
+                    travelTo: controls.travelTo,
+                    span: controls.selectMode
+                  }} />
               </TimeContainer>
             </Container>
           )
--- src/calendar/time-span/index.js
+++ src/calendar/time-span/index.js
@@ -4,81 +4,161 @@ import Grid from '../grid'
 import Item from '../item'
 
 import {
-  buildCalendar,
+  build,
   weekdays
 } from '../../lib/calendar-fns'
 import {
-  format
+  format,
+  monthValues
 } from '../../lib/date-fns'
 
 class TimeSpan extends React.Component {
   constructor (props) {
     super(props)
 
-    this._handleClick = this._handleClick.bind(this)
-    this._handleNextClick = this._handleNextClick.bind(this)
-    this._handlePreviousClick = this._handlePreviousClick.bind(this)
-    this._handleModeClick = this._handleModeClick.bind(this)
+    this.state = {
+      spans: ['month', 'year', 'decade']
+    }
+
+    this.handleClick = this.handleClick.bind(this)
+    this.handleNextClick = this.handleNextClick.bind(this)
+    this.handlePreviousClick = this.handlePreviousClick.bind(this)
+    this.handleModeClick = this.handleModeClick.bind(this)
   }
 
-  _handleClick (props) {
-    this.props.clickMethod({...props, mode: 'month'})
+  handleClick (props) {
+    const index = this.state.spans.indexOf(this.props.span)
+    const newIndex = index === 0 ? 0 : index - 1
+    this.props.controls.select({...props, mode: this.state.spans[newIndex]})
   }
 
-  _handleNextClick () {
-    this.props.travelTo('next', 'month')
+  handleNextClick () {
+    this.props.controls.travelTo('next', this.props.span)
   }
 
-  _handlePreviousClick () {
-    this.props.travelTo('previous', 'month')
+  handlePreviousClick () {
+    this.props.controls.travelTo('previous', this.props.span)
   }
 
-  _handleModeClick () {
-    this.props.modeMethod('year')
+  handleModeClick () {
+    const index = this.state.spans.indexOf(this.props.span)
+
+    this.props.controls.span(this.state.spans[index + 1])
   }
 
   items () {
-    const days = []
+    const units = []
 
-    weekdays().forEach((weekday, i) => {
-      days.push(<Item label={weekday} />)
-    })
+    if (this.props.span === 'month') {
+      weekdays().forEach((weekday, i) => {
+        units.push(<Item label={weekday} />)
+      })
+    }
 
-    buildCalendar(this.props.startDate).forEach((date) => {
+    build(this.props.span, this.props.startOfCalendar).forEach((date) => {
       const dateString = format(date, 'YYYY-MM-DD')
-      const currentDate = format(this.props.currentDate, 'YYYY-MM-DD')
-      days.push(
+      const startOfMonth = format(this.props.startOfMonth, 'YYYY-MM-DD')
+      let label, id, selected, muted, focused
+      switch (this.props.span) {
+        case 'decade':
+          id = format(date, 'YYYY')
+          label = format(date, 'YYYY')
+          selected = (this.props.selected || '').slice(0, 4) === id
+          muted = startOfMonth.slice(0, 3) !== id.slice(0, 3)
+          focused = (this.props.today || '').slice(0, 4) === id
+          break
+        case 'year':
+          const monthNames = monthValues.abbreviated
+          id = dateString.slice(0, 7)
+          label = monthNames[date.getMonth()]
+          selected = (this.props.selected || '').slice(0, 7) === id
+          muted = startOfMonth.slice(0, 4) !== id.slice(0, 4)
+          focused = (this.props.today || '').slice(0, 7) === id
+          break
+        default:
+          id = dateString
+          label = date.getDate()
+          selected = this.props.selected === id
+          muted = startOfMonth.slice(0, 7) !== id.slice(0, 7)
+          focused = this.props.today === id
+      }
+      units.push(
         <Item
-          id={dateString}
-          clickMethod={this._handleClick}
+          id={id}
+          clickMethod={this.handleClick}
           date={date}
-          focused={this.props.today === dateString}
-          muted={currentDate.slice(0, 7) !== dateString.slice(0, 7)}
-          selected={this.props.selected === dateString}
-          label={date.getDate()} />
+          focused={focused}
+          muted={muted}
+          selected={selected}
+          label={label} />
       )
     })
 
-    return days
+    return units
+  }
+
+  calcSize () {
+    let itemWidth, widthUnit, itemsWide
+
+    switch (this.props.span) {
+      case 'decade':
+      case 'year':
+        itemWidth = 4.375
+        widthUnit = 'em'
+        itemsWide = 4
+        break
+      default:
+        itemWidth = 2.5
+        widthUnit = 'em'
+        itemsWide = 7
+    }
+    return {
+      itemWidth,
+      widthUnit,
+      itemsWide
+    }
+  }
+
+  buttonLabel () {
+    switch (this.props.span) {
+      case 'decade':
+        const startYear = this.props.startOfMonth.getFullYear()
+        const decade = startYear - startYear % 10
+
+        return `${decade} - ${decade + 9}`
+      case 'year':
+        return this.props.startOfMonth.getFullYear()
+      default:
+        return format(this.props.startOfMonth, 'MM, YYYY')
+    }
   }
 
   render () {
+    const sizes = this.calcSize()
     return (
       <div>
         <div>
-          <button onClick={this._handlePreviousClick}> {'<'} </button>
-          <button onClick={this._handleModeClick}>{format(this.props.currentDate, 'MM, YYYY')}</button>
-          <button onClick={this._handleNextClick}> {'>'} </button>
+          <button onClick={this.handlePreviousClick}> {'<'} </button>
+          <button onClick={this.handleModeClick}>{this.buttonLabel()}</button>
+          <button onClick={this.handleNextClick}> {'>'} </button>
         </div>
-        <Grid items={this.items()} itemWidth={2.5} widthUnit={'em'} itemsWide={7} />
+        <Grid items={this.items()} itemWidth={sizes['itemWidth']} widthUnit={sizes['widthUnit']} itemsWide={sizes['itemsWide']} />
       </div>
     )
   }
 }
 
 TimeSpan.propTypes = {
-  startDate: PropTypes.instanceOf(Date).isRequired,
-  clickMethod: PropTypes.func
+  span: PropTypes.string.isRequired,
+  startOfMonth: PropTypes.instanceOf(Date).isRequired,
+  startOfCalendar: PropTypes.instanceOf(Date).isRequired,
+  selected: PropTypes.string,
+  today: PropTypes.string,
+  controls: PropTypes.shape({
+    select: PropTypes.func,
+    span: PropTypes.func,
+    travelTo: PropTypes.func
+  })
 }
 
 export default TimeSpan
