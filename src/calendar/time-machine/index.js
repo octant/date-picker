fbe05db770da4d37fda719b607e8ee7ee40db120
Michael Wood <mdwood@algomapublichealth.com>
2018-04-03 16:29:43 -0400

    Fixed component name

--- src/calendar/time-machine/index.js
+++ src/calendar/time-machine/index.js
@@ -5,7 +5,7 @@ import {
   get as calendarGet
 } from '../../lib/calendar-fns'
 
-class TimeTraveler extends React.Component {
+class TimeMachine extends React.Component {
   constructor (props) {
     super(props)
 
@@ -75,4 +75,4 @@ class TimeTraveler extends React.Component {
   }
 }
 
-export default TimeTraveler
+export default TimeMachine
