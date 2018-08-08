6c4de4b7991f5dd7a9ac7cd2bb1174bb7065b254
Michael Wood <mdwood@algomapublichealth.com>
2018-08-08 10:49:05 -0400

    placed today in its own div

--- src/calendar/index.js
+++ src/calendar/index.js
@@ -4,6 +4,7 @@ import { Container } from "./styles";
 import TimeContainer from "./time-container";
 import TimeMachine from "./time-machine";
 import TimeSpan from "./time-span";
+import { format } from "date-fns";
 
 class Calendar extends React.Component {
   render() {
@@ -20,7 +21,11 @@ class Calendar extends React.Component {
           } = tmState;
           return (
             <Container>
-              <button onClick={tmState.controls.today}>Today</button>
+              <div style={{ padding: `0.5em 0.5em 0 0.5em` }}>
+                <button onClick={tmState.controls.today}>
+                  {format(today, "dddd, MMMM D, YYYY")}
+                </button>
+              </div>
               <TimeContainer>
                 <TimeSpan
                   span={mode}
